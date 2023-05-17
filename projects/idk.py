98fe642b61de3a5912fdf0a547a9a965a5e9b380
vintageblu <94313833+vintageblu@users.noreply.github.com>
2023-05-17 09:49:08 -0400

    Create idk.py

---
+++ projects/idk.py
@@ -0,0 +1,15 @@
+# idk what to name this file 😭😭, but yeah, it's basically asking for your name and matching it to the original input i have given it.
+
+realName = "ABCDE"
+realPassword = "password132"
+
+print("Enter Username")
+name = (input())
+
+print("Enter Password")
+password = (input())
+
+if realName == name and realPassword == password:
+  print("You have logged in")
+else:
+  print("Wrong Username or Password")
